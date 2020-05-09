34421e14c3586ec61f45188cc267f814e0e8ac8f
Aditya <39548065+aditya-pandey@users.noreply.github.com>
2020-05-09 18:29:50 +0530

    Add files via upload

---
+++ Book/book.js
@@ -0,0 +1,46 @@
+var indexI =document.querySelector(".index-i");
+var fontI = document.querySelector(".font-i");
+var themeI = document.querySelector(".theme-i");
+var index = document.querySelector(".index");
+var fontProp = document.querySelector(".font-prop");
+var theme = document.querySelector(".theme");
+var themechanger = document.querySelectorAll(".theme button");
+var cover = document.querySelector(".cover");
+
+var font = document.querySelector("#fonts");
+var paper = document.querySelectorAll(".paper");
+var paperP = document.querySelectorAll(".paper p");
+
+var icons = [indexI,fontI,themeI];
+var features = [index,fontProp,theme];
+
+for(let i=0;i<icons.length;i++){
+    icons[i].addEventListener("click", () => {
+        if(features[i].style.display == "flex"){
+            features[i].style.display = "none";
+        }
+        else{
+            features[i].style.display = "flex";
+        }
+        for (let j = 0; j < icons.length; j++) {
+            if(j != i){
+                features[j].style.display = "none";
+            }
+        }
+        ;
+    })
+}
+themechanger.forEach(el => {
+    el.addEventListener("click",() => {
+        for (let i = 1; i < paper.length; i++) {
+            paper[i].style.background = `${el.style.background}`;
+            paper[i].style.color = `${el.style.color}`;
+        }
+    })
+})
+
+font.addEventListener("change",() => {
+    paperP.forEach(el => {
+        el.style.fontFamily = `${font.value}`;
+    });
+})
---
+++ loader.js
@@ -0,0 +1,32 @@
+setTimeout(()=>{
+    var preload = document.querySelector(".preload");
+    var name = document.querySelector(".preload h2");
+    var loading = document.querySelector(".preload h3");
+    var preloadLogo = document.querySelector(".preload img");
+    var preloadBook = document.querySelector(".preload-book");
+    var poemIntro = document.querySelector(".poem-intro");
+    var about = document.querySelector("#about");
+    var contact = document.querySelector("#contact");
+    // name.style.transform = "translateY(-500px)";
+    // preloadBook.style.transform = "translateY(500px)";
+    name.style.opacity= "0";
+    loading.style.opacity = "0";
+    preloadBook.style.opacity= "0";
+    preload.style.opacity = "0";
+    preloadLogo.style.opacity = "0";
+    setTimeout(function(){
+        preload.style.display = "none";
+    },
+    2000)
+},5000);
+
+(function(){
+    var burger = document.querySelector('.burger-container');
+    var header = document.querySelector('.navbar');
+
+    burger.onclick = function() {
+        header.classList.toggle('menu-opened');
+    }
+    
+}());
+
